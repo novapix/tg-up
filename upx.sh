498644846b58345a6fb3e4ea43ad4bfc85d74e63
Parakram <parakram@keemail.me>
2025-12-12 14:31:00 +0545

    avoid compressing mac build

--- upx.sh
+++ upx.sh
@@ -3,19 +3,27 @@ set -e
 
 DIST_DIR=dist
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./upx.sh <GOOS>"
+    exit 1
+fi
+
+GOOS="$1"
+
 # Check if UPX is installed and accessible
 if ! command -v upx &> /dev/null; then
-    echo "ERROR: UPX not found!"
-    echo "Please ensure 'upx-ucl' or 'upx' is installed and available in your system's PATH."
+    echo "ERROR: UPX not found! Please install it first."
     exit 1 
 fi
 
-echo "Compressing binaries in $DIST_DIR using fast compression..."
+UPX_OPTS="--fast"
+
+echo "Compressing binaries in $DIST_DIR for $GOOS using options: $UPX_OPTS"
 
 for bin in "$DIST_DIR"/*; do
     if [[ -f "$bin" ]]; then
         echo "Compressing $bin..."
-        upx --fast "$bin"
+        upx $UPX_OPTS "$bin"
     fi
 done
 
